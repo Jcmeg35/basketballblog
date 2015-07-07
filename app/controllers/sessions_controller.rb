6208f1ed6591c8478d11dc5ac63e9a450daa16cc
Jacob <Jacobemeier@gmail.com>
2015-07-07 15:32:14 -0400

    First commit to github

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,32 @@
+class SessionsController < ApplicationController
+
+  def destroy
+    session[:user_id] = nil
+    flash[:alert] = "Successfully Logged Out"
+    redirect_to root_path
+  end
+
+  def create
+    username = params[:username]
+    password = parmas[:password]
+  
+    @user = User.where(username: username).first
+
+    if @user.nil?
+      #wrong username case
+      flash[:alert] = "Incorrect credentials"
+      redirect_to root_path
+    else
+      if @user.password == password
+        session[:user_id] = @user.id
+        flash[:alert] = "Welcome!"
+        redirect_to root_path
+        #succes case
+      else 
+        #wrong password case
+        flash[:alert] = "Incorrect credentials"
+        redirect_to root_path
+      end
+    end
+  end
+end 
