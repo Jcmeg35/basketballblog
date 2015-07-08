11100cb12a3a497d18322630ef3d022e5d8939c9
Jacob <Jacobemeier@gmail.com>
2015-07-07 20:37:21 -0400

    added post controller code and .erb files.  Issue with post table.  Schema is not picking up body and title from post table.  need to look into further.:

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,18 @@
 class PostsController < ApplicationController
+
+  def index
+    @posts = Posts.all
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @user = current_user
+    @post = Post.create params[:post]
+    flash[:alert] = "You made a new post!"
+    #redirect_to "feed"
+    #render: "name of the view" (this would re-render.  Can use a string or symbol)
+  end
 end
