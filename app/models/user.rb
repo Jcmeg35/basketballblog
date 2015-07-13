5ef00daae1007bd420d1a8ff87bb4c6a9a4c2a79
Jacob <Jacobemeier@gmail.com>
2015-07-13 16:35:05 -0400

    finished styling comments and posts pages

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ActiveRecord::Base
+  validates_presence_of :username, :password;
+  validates :password, confirmation: true,  presence: true
+  validates :username, uniqueness: true
+
   has_many :posts
   has_many :comments
 end
